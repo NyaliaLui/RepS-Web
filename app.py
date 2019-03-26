086c39067c6548173764435df4786c19309e485c
Nyalia Lui <luinyalia@gmail.com>
2019-03-25 22:41:08 -0400

    added premade archive but so far only works with hardcoding the AWS credentials into python script. We DO NOT want this, so we removed those lines of code and need to determine how to get this working on AWS EC2 redhat linux

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ from werkzeug.utils import secure_filename
 from reps import Dispatcher
 import logging
 import sys
-import json, boto3
+import boto3
 from botocore.client import Config
+import botocore
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 ARCHIVE_MANAGER = Dispatcher(APP_ROOT)
 UPLOAD_FOLDER = os.path.join(APP_ROOT, 'uploads')
 REPLAY_FOLDER = os.path.join(APP_ROOT, 'replays')
+ARCHIVE_FOLDER = os.path.join(APP_ROOT, 'archive')
 REPLAYS_ZIP = 'Replays.zip'
 ALLOWED_EXTENSIONS = set(['zip'])
 
@@ -23,6 +25,45 @@ app.logger.setLevel(logging.ERROR)
 def valid_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def create_subfolders(directory):
+    os.chdir(ARCHIVE_FOLDER)
+
+    try:
+        os.mkdir(directory)
+    except OSError:
+        print(directory + ' already created')
+
+    try:
+        os.mkdir(os.path.join(directory, 'M'))
+    except OSError:
+        print(directory + '/M already created')
+
+    try:
+        os.mkdir(os.path.join(directory, 'P'))
+    except OSError:
+        print(directory + '/P already created')
+
+    os.chdir(APP_ROOT)
+
+def transfer_from_s3(archive_name, local_dest):
+    os.chdir(local_dest)
+
+    S3_BUCKET = os.getenv('S3_BUCKET')
+    s3 = boto3.client('s3', region_name='us-east-2', config=Config(signature_version='s3v4'))
+
+    try:
+        s3.download_file(S3_BUCKET, archive_name, archive_name)
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print(archive_name + " does not exist on S3.")
+        else:
+            raise
+    except TypeError as ex:
+        print(S3_BUCKET, type(S3_BUCKET))
+        print(archive_name, type(archive_name))
+
+    os.chdir(APP_ROOT)
 
 @app.route("/")
 def home():
@@ -107,7 +148,32 @@ def help():
 def about():
     return render_template("about.html")
 
+@app.route("/premade")
+def premade():
+    return render_template("premade.html")
+
+@app.route("/archive/<directory>/<sortop>/Replays", methods=['GET', 'POST'])
+def send_archive(directory, sortop):
+
+    if request.method == 'GET':
+        location = os.path.join(ARCHIVE_FOLDER, directory, sortop)
+        zip_file = directory + '-' + 'SB' + sortop + '.zip'
+
+        if not os.path.isfile(os.path.join(location, zip_file)):
+            create_subfolders(directory)
+            transfer_from_s3(zip_file, location)
+            print('archives successfully transfered')
+
+        return send_from_directory(location, zip_file)
+
+    return render_template("nofile.html")
+
 if __name__ == "__main__":
+    try:
+        os.mkdir(ARCHIVE_FOLDER)
+    except:
+        print('archive already exists')
+    
     try:
         os.mkdir(UPLOAD_FOLDER)
     except:
