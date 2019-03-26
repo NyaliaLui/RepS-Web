b4f3392ae2ce1320982226b833a990e72998e583
Nyalia Lui <luinyalia@gmail.com>
2019-03-25 20:58:32 -0400

    brought in translator

--- app.py
+++ app.py
@@ -1,26 +1,16 @@
 import os
 from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory
-<<<<<<< HEAD
-=======
 from werkzeug.utils import secure_filename
 from reps import Dispatcher
->>>>>>> 25b3f731a39d6f612e48b248f9d7ce9381be3810
 import logging
 import sys
-import boto3
+import json, boto3
 from botocore.client import Config
-import botocore
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-<<<<<<< HEAD
-UPLOAD_FOLDER = os.path.join(APP_ROOT, os.path.join('tmp', 'uploads'))
-REPLAY_FOLDER = os.path.join(APP_ROOT, os.path.join('tmp', 'replays'))
-ARCHIVE_FOLDER = os.path.join(APP_ROOT, os.path.join('tmp', 'archive'))
-=======
 ARCHIVE_MANAGER = Dispatcher(APP_ROOT)
 UPLOAD_FOLDER = os.path.join(APP_ROOT, 'uploads')
 REPLAY_FOLDER = os.path.join(APP_ROOT, 'replays')
->>>>>>> 25b3f731a39d6f612e48b248f9d7ce9381be3810
 REPLAYS_ZIP = 'Replays.zip'
 ALLOWED_EXTENSIONS = set(['zip'])
 
@@ -30,51 +20,14 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
-def create_subfolders(directory):
-    os.chdir(ARCHIVE_FOLDER)
+def valid_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-<<<<<<< HEAD
-    try:
-        os.mkdir(directory)
-    except OSError:
-        print(directory + ' already created')
-
-    try:
-        os.mkdir(os.path.join(directory, 'M'))
-    except OSError:
-        print(directory + '/M already created')
-
-    try:
-        os.mkdir(os.path.join(directory, 'P'))
-    except OSError:
-        print(directory + '/P already created')
-
-    os.chdir(APP_ROOT)
-
-def transfer_from_s3(archive_name, local_dest):
-    os.chdir(local_dest)
-
-    S3_BUCKET = os.environ.get('S3_BUCKET')
-    s3 = boto3.client('s3', region_name='us-east-2', config=Config(signature_version='s3v4'))
-
-    try:
-        s3.download_file(S3_BUCKET, archive_name, archive_name)
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == "404":
-            print(archive_name + " does not exist on S3.")
-        else:
-            raise
-
-    os.chdir(APP_ROOT)
-
-=======
->>>>>>> 25b3f731a39d6f612e48b248f9d7ce9381be3810
 @app.route("/")
 def home():
     return render_template("home.html")
 
-<<<<<<< HEAD
-=======
 @app.route("/sort/player", methods=['GET', 'POST'])
 def org_player():
     if request.method == 'POST':
@@ -150,40 +103,11 @@ def thankyou(directory):
 def help():
     return render_template("help.html")
 
->>>>>>> 25b3f731a39d6f612e48b248f9d7ce9381be3810
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-<<<<<<< HEAD
-@app.route("/premade")
-def premade():
-    return render_template("premade.html")
-
-@app.route("/archive/<directory>/<sortop>/Replays", methods=['GET', 'POST'])
-def download(directory, sortop):
-
-    if request.method == 'GET':
-        location = os.path.join(ARCHIVE_FOLDER, directory, sortop)
-        zip_file = directory + '-' + 'SB' + sortop + '.zip'
-
-        if not os.path.isfile(os.path.join(location, zip_file)):
-            create_subfolders(directory)
-            transfer_from_s3(zip_file, location)
-            print('archives successfully transfered')
-
-        return send_from_directory(location, zip_file)
-
-    return render_template("nofile.html")
-
-=======
->>>>>>> 25b3f731a39d6f612e48b248f9d7ce9381be3810
 if __name__ == "__main__":
-    try:
-        os.mkdir(ARCHIVE_FOLDER)
-    except:
-        print('archive already exists')
-
     try:
         os.mkdir(UPLOAD_FOLDER)
     except:
--- reps/Tests/unittests.py
+++ reps/Tests/unittests.py
@@ -3,6 +3,7 @@ from replay_tests import ReplayTestCase
 from inspector_tests import InspectorTestCase
 from folder_processor_tests import FolderProcessorTestCase
 from dispatcher_tests import DispatcherTestCase
+from translator_tests import TranslatorTestCase
 
 if __name__ == '__main__':
     unittest.main()
